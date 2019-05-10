6935099b98e6d4054fdd42e4c441c658ab4f2918
greenskybluephish <greenskybluephish@gmail.com>
2019-05-10 15:53:19 -0500

    Updates

---
+++ precache-manifest.38a9b1b7dd7a48dbad4ef713c8b23efc.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378604bf52ddf6df93bc2f6613a6975a",
+    "url": "/spotify-frontend/index.html"
+  },
+  {
+    "revision": "853843bca8497b680381",
+    "url": "/spotify-frontend/static/css/2.f4f8339c.chunk.css"
+  },
+  {
+    "revision": "90f3e0e537d3d91b2282",
+    "url": "/spotify-frontend/static/css/main.3a873b5d.chunk.css"
+  },
+  {
+    "revision": "853843bca8497b680381",
+    "url": "/spotify-frontend/static/js/2.be66ed69.chunk.js"
+  },
+  {
+    "revision": "90f3e0e537d3d91b2282",
+    "url": "/spotify-frontend/static/js/main.a2e762ae.chunk.js"
+  },
+  {
+    "revision": "63ddd0b5cdd0a257d5f2",
+    "url": "/spotify-frontend/static/js/runtime~main.8f6556f8.js"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/spotify-frontend/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/spotify-frontend/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/spotify-frontend/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/spotify-frontend/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/spotify-frontend/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/spotify-frontend/static/media/square5.ff004a0e.png"
+  }
+]);
